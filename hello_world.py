74786d38288f15d8b7ee0b09914216cd6c717315
Adri Hilmie Mohd. Radzi <75724080+AdriHilmie29@users.noreply.github.com>
2021-01-10 16:56:04 +0800

    Start using Python by printing Hello World!

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+#after you succesfully ran your Python IDE, please configure with your Python newest software so
+#you will not miss any newest updates.
+
+#to check wheter your IDE works fine, enter command below:
+
+print('Hello World!')
+
+#click run or use shift+F10
+#remember to put '' at your statement because it is your defined variable which will be your output.
